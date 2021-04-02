f64157b9278cbdee7bff50c25d9858ad04b0e7cb
Eric Marnadi <ericmarnadi@outlook.com>
2021-04-01 20:44:44 -0700

    Update Attack.py
    Need to call place_scouts method each turn

--- python-algo/Attack.py
+++ python-algo/Attack.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  1 01:01:50 2021
+def __init__(self):
+    super().__init__()
+    seed = random.randrange(maxsize)
+    random.seed(seed)
+    gamelib.debug_write('Random seed: {}'.format(seed))
+    MUlocs = {"left":[[3, 10], [4, 9]], "right":[[24, 10], [23, 9]]}
+    MUthreshold = 15
+    MUmultiplier = 0.99
 
-@author: alrick
-"""
-
-"""
-Plan for the attach phase.
-"""
+def place_scouts(self, game_state):
+    if (game_state.MP > self.MUthreshold):
+        game_state.attempt_spawn(SCOUT, self.MUlocs["left"][0], game_state.MP / 2)
+        game_state.attempt_spawn(SCOUT, self.MUlocs["left"][1], game_state.MP / 2)
+    self.MUthreshold *= self.MUmultiplier
